9d92fdd4a7be70fa708b3cf4b9dc0f4107041ac9
Sagar Dhone <sdhone98@gmail.com>
2024-06-06 22:29:00 +0530

    Fix

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -7,35 +7,58 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 import "./Home.scss";
+import Switch from '@mui/material/Switch';
 
 const Home = () => {
+  const [isDayMode, setIsDayMode] = useState(false)
+  const [isDayModeFlag, setIsDayModeFlag] = useState("Light")
   const [phoneNumber, setPhoneNumber] = useState(0);
   const [message, setMessage] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState(false);
   const theme = createTheme({
     palette: {
       customGreen: {
         main: "#075e54",
       },
-    },
+    },  
   });
+
+  const toggleMode = () => {
+    setIsDayMode(!isDayMode)
+    setIsDayModeFlag(isDayMode? "Light" : "Dark")
+    console.log("===> ",isDayMode)
+  }
 
   const onPhoneNumberChanged = (phoneNumber, country) => {
     setPhoneNumber(phoneNumber);
   };
+  const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
   const sendMessage = () => {
-    if (phoneNumber) {
-      const splitPhoneNumber = phoneNumber.split(" ");
-      const _phoneNumber = splitPhoneNumber[1].replace("-", "");
-      const url = `https://wa.me/${_phoneNumber}?text=${
-        message ? message : "Hello..."
-      }`;
-      const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    const url = `https://wa.me/${phoneNumber}?text=${
+      message ? message : "Hello..."
+    }`;
+
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  const checkPhoneNumber = (e) => {
+    console.log(e.target.value);
+    if (e.target.value.length == 10) {
+      setPhoneNumberError(false);
+      setPhoneNumber(e.target.value);
+    } else {
+      setPhoneNumberError(true);
     }
   };
 
   return (
     <div className="main-home">
+      {/* <div className="mode-switch">
+
+      <Switch className="switch-btn" {...label} defaultChecked onClick={toggleMode}/>
+      <span>{isDayModeFlag}</span>
+      </div> */}
       <ThemeProvider theme={theme}>
         <Box
           className="content"
@@ -49,10 +72,15 @@ const Home = () => {
           m={0}
         >
           <h2 className="text">Send WhatsApp Message.</h2>
-          <MuiPhoneNumber
-            className="phone-number"
-            defaultCountry="in"
-            onChange={onPhoneNumberChanged}
+          <TextField
+            error={phoneNumberError}
+            helperText={phoneNumberError && "invalid phone number."}
+            id="outlined-basic"
+            label="Phone Number"
+            variant="outlined"
+            type="number"
+            color="customGreen"
+            onChange={checkPhoneNumber}
           />
           <TextField
             fullWidth
